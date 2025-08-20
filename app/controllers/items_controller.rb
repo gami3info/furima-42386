c43683fab3ce6244e0dd2ade713b5c2bc3145b3c
gami3info <mugennotsubasa374@gmail.com>
2025-08-20 21:14:31 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.with_attached_image.includes(:purchase).order('created_at DESC')
@@ -19,8 +19,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # end
+  def show
+  end
 
   private
 
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
